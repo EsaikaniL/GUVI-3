27320d44a47ae2d5229531db033292172aaf3f3b
tiruajancs <31504425+tiruajancs@users.noreply.github.com>
2018-01-11 10:19:12 +0000

    Create string.c

---
+++ string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    string arr[] = {"geeksforgeeks", "geeks",
+                    "geek", "geezer"};
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    string ans = commonPrefix(arr, n);
+ 
+    if (ans.length())
+        printf ("The longest common prefix is - %s",
+                 ans.c_str());
+    else
+        printf("There is no common prefix");
+}
