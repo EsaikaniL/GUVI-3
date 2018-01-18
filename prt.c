c35ff386f92322e920d1ce7b583abae66c5da546
tiruajancs <31504425+tiruajancs@users.noreply.github.com>
2018-01-18 10:51:37 +0000

    Create prt.c

---
+++ prt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char t[30];
+char k;
+int i;
+clrscr();
+scanf("%s",t);
+for(i=0;t[i]!=NULL;i++)
+{
+if(t[i]==k)
+{
+printf("the position is:%d",i);
+}
+}
+getch();
+}
