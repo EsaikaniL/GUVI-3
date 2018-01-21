15488702893fd6ade6978711edb55ab56750bc04
tiruajancs <31504425+tiruajancs@users.noreply.github.com>
2018-01-21 13:59:52 +0530

    Create karthee.c

---
+++ karthee.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void)
+{
+	int z,x,flag=0,count=0;
+	int t,k;
+	scanf("%d %d",&z,&x);
+	for(k=z;k<=x;k++)
+	{
+	for(t=2;t<k;t++)
+	{
+		if(k%t==0)
+		{
+			flag=1;
+			break;
+		}
+		else
+		{
+			flag=0;
+		}
+	}
+	if(flag==0)
+	{
+		count=count+1;
+	}
+	}
+	printf("%d",count);
+
+	return 0;
+}
