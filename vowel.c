f314a779b3398613e473ce7b8893443412bedceb
tiruajancs <31504425+tiruajancs@users.noreply.github.com>
2018-01-21 14:17:17 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char=15;
+printf("enter any word");
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+printf("character is vowel");
+}
+else
+{
+printf("character is constant"):
+}
+}
