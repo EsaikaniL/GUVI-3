31be3b1f8777fa4d4be539a2ceb5bb070f5cf232
tiruajancs <31504425+tiruajancs@users.noreply.github.com>
2018-01-21 14:08:44 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int k;
+printf("enter any integer");
+scanf("%d",&n);
+if(n<=0)
+{
+(if(n==0)
+{
+printf("number is zero");
+}
+else
+{
+printf("number is integer");
+}
+}
+else
+{
+printf("number is positive");
+}
+return 0;
+}
