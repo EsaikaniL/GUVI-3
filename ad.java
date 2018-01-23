1a7589ba4175e2a462b44ce58887ed5ca0e392d6
tiruajancs <31504425+tiruajancs@users.noreply.github.com>
2018-01-23 16:19:42 +0530

    Create ad.java

---
+++ ad.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class repeated
+{
+  public static void main(String[] args) 
+  {
+		Scanner sc=new Scanner(System.in);
+		String s[]=sc.nextLine().split(" ");
+		for(int t=0;t<s.length-1;t++)
+		{
+			for(int j=0;j<s.length-1;j++)
+			{
+			if(s[i].equals(s[j+1]))
+			{
+				System.out.print(s[i]);
+				System.exit(00);
+			}
+			}
+		}
+	}
+}
