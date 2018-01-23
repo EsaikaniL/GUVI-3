c102cf490f12d4918eee46fdab9ab9fe966c20c1
tiruajancs <31504425+tiruajancs@users.noreply.github.com>
2018-01-23 16:57:01 +0530

    Create a2a.c

---
+++ a2a.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+char a[1i]="love";int b;
+b=strlen(a);
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%c",a[b-1]);
+b--;
+}
+return 0;
+}
