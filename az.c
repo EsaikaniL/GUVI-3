62d213bedc0e5fe75b07527b5abef8cb5b19dabe
tiruajancs <31504425+tiruajancs@users.noreply.github.com>
2018-01-23 15:59:42 +0530

    Create az.c

---
+++ az.c
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<conio.h>
+#include <string>
+int main ()
+{
+  std::string str;
+  std::string str2="erode";
+  str.append(str2);                     
+  str.append(1u,'.');                    
+  return 0;
+}
