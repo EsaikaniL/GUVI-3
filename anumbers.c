13a94e1c4847a6e4003e2d7e5da6b096f1b21f1b
tiruajancs <31504425+tiruajancs@users.noreply.github.com>
2018-01-25 15:57:39 +0530

    Create anumbers.c

---
+++ anumbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int N,K,t,a[55],b[55],j;
+scanf("%d %d",&N,&K);
+for(t=0;t<N;t++)
+{
+scanf("%d",&a[t]);
+}
+for(t=0;t<N;t++)
+{
+for(j=t1;j<=N;j++)
+{
+if(a[t%2!=0)
+{
+b[j]=a[t;
+}
+}
+}
+printf("%d",b[k-1]);
+getch();
+}
