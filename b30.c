e7e9c5812bcc6e36c4f2f8aa7940379d03277503
AJan tk <31504425+tiruajancs@users.noreply.github.com>
2018-02-27 11:10:08 +0000

    Rename a30.c to b30.c

---
+++ b30.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+	int t,k,l,v,e,u;
+	scanf("%d",&t);
+	scanf("%d",&k);
+	scanf("%d",&l);
+	scanf("%d",&v);
+    e=t-l;
+    u=k-v;
+	printf("%d %d",e,u);
+	return 0;
+}
