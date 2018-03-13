236abd84081e9b018d9069ff3319f6a001344d2d
AJan t <31504425+tiruajancs@users.noreply.github.com>
2018-03-13 11:05:06 +0000

    Create aa13.c

---
+++ aa13.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the a value");
+    scanf("%d",&a);
+    printf("enter the b value");
+    scanf("%d",&b);
+    c=(a*a)+(b*b);
+    printf("sum of suqares %d",c);
+    return 0;
+}
